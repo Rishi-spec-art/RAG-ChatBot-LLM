f67b8b726bd445acc02395c6bf76a52ef449a1a7
Rishi-spec-art <80808886+Rishi-spec-art@users.noreply.github.com>
2024-04-07 13:35:12 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -14,7 +14,7 @@ class chatModel:
         ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
         self.tokenizer = AutoTokenizer.from_pretrained(model_id, token = ACCESS_TOKEN, cache_dir = CACHE_DIR)
         
-        self.model = AutoModelForCausalLM.from_pretrained(model_id)
+        self.model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir = CACHE_DIR, token = ACCESS_TOKEN)
         self.model.eval()
         self.device = device
         self.chat = []
