57485998aeec19657d1bd39ec57c5abb9982ed40
Rishi-spec-art <80808886+Rishi-spec-art@users.noreply.github.com>
2024-04-07 11:49:52 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import os
-# load_dotenv()
+load_dotenv()
 
 CACHE_DIR = os.path.normpath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "files")
@@ -11,8 +11,8 @@ CACHE_DIR = os.path.normpath(
 class chatModel:
     def __init__(self, ac_token, model_id:str = "google/gemma-2b-it", device = 'gpu'):
 
-        # ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id, token = ac_token, cache_dir = CACHE_DIR)
+        ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id, token = ACCESS_TOKEN, cache_dir = CACHE_DIR)
         
         self.model = AutoModelForCausalLM.from_pretrained(model_id)
         self.model.eval()
